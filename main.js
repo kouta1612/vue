5087185583192eb540b89f2d261654288473cfb3
kouta1612 <kouta1612world69@gmail.com>
2019-04-30 18:41:44 +0900

    complete #14

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+}
+
 Vue.component('coupon', {
     template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
     methods: {
         onCouponApplied() {
-            this.$emit('applied');
+            Event.fire('applied');
         }
     }
 });
 
 new Vue({
     el: '#root',
-
     data: {
         couponApplied: false
     },
-
-    methods: {
-        onCouponApplied() {
-            this.couponApplied = true
-        }
+    created() {
+        Event.listen('applied', () => alert('Handling it!'));
     }
 });
